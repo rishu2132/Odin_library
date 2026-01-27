949543bc43298dea7c0b60f7bb215ad9bf707118
Utkarsh <utk1298@gmail.com>
2026-01-28 01:53:14 +0530

    book constructor and function to add the book

---
+++ script.js
@@ -0,0 +1,25 @@
+const myLibrary =[];
+
+function Book(title,author,pages,read){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID();
+}
+
+function addBooktoLibrary(){
+    const book = new Book ('me','your',102,'read')
+    const book2 = new Book('your','me',12,'read')
+    myLibrary.push(book)
+    myLibrary.push(book2)
+
+}
+
+addBooktoLibrary();
+console.log(myLibrary)
+const body = document.querySelector("body");
+
+myLibrary.forEach((book)=>{
+    body.innerText =body.innerText + book;
+})
